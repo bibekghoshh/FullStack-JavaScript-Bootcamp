cd53a850a1914c3c4ac9e4a82d7578dcd55b2c7e
bibekghosh <bibek7478@gmail.com>
2023-04-08 21:30:21 +0530

    updated

--- Airticles/readme.md
+++ Airticles/readme.md
@@ -1,4 +1,5 @@
 # My Airticles are link below
 
 1. [Basic introduction to web and html](https://bibekghosh.hashnode.dev/basic-introduction-to-web-and-html)
-2. [Learn about CSS selector]()
+2. [Learn about CSS selector](https://bibekghosh.hashnode.dev/lets-learn-about-css-selector)
+3. [How to Create our first Express Js App](https://bibekghosh.hashnode.dev/how-to-create-your-first-express-app)
---
+++ BACKEND/Practices/firstapp/index.js
@@ -0,0 +1,36 @@
+const express=require('express'); //bringing the express
+// const res = require('express/lib/response');
+
+const app=express(); //assgin the express into app
+
+const port=3000; //assigning the port number
+
+app.get("/",(req,res)=>{
+    res.status(201).send("Hey We are learning Exprexx FRamework");
+})
+
+app.get("/bibek",(req,res)=>{
+    const bibek={
+        firstname: "Bibek",
+        Lastname: "Ghosh",
+        age: 23,
+        location: "India"
+    };
+    res.status(404).send({bibek});
+});
+
+app.get("/Insta",(req,res)=>{
+    const insta={
+        username:"Express framework",
+        location:"Bengal",
+        age:23,
+        education:"Master's of computer Application"
+    }
+    res.status(201).json({insta});
+});
+
+app.listen(port,()=>{
+    console.log("Server Running");
+})
+
+
